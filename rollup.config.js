a81bb41aa5d6efd8a429f402bbc5c4dd9c4e6c4f
Surya Sankar <suryashankar.m@gmail.com>
2020-10-27 01:20:35 +0530

    changing output file names

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,34 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import { terser } from "rollup-plugin-terser";
+import scss from 'rollup-plugin-scss'
 import pkg from './package.json';
 
-export default [
-    // browser-friendly UMD build
-    {
-        input: 'src/index.js',
-        output: {
+export default {
+    input: 'src/index.js',
+    external: ['ms'],
+    plugins: [
+        resolve(), // so Rollup can find `ms`
+        commonjs(),// so Rollup can convert `ms` to an ES module
+        terser(),
+        scss({
+            output: 'dist/databuddy-client.min.css',
+            outputStyle: "compressed"
+        }),
+    ],
+    output: [
+        {
             name: 'databuddy-client',
             file: pkg.browser,
-            format: 'umd'
+            format: 'umd',
         },
-        plugins: [
-            resolve(), // so Rollup can find `ms`
-            commonjs() // so Rollup can convert `ms` to an ES module
-        ]
-    },
-
-    // CommonJS (for Node) and ES module (for bundlers) build.
-    // (We could have three entries in the configuration array
-    // instead of two, but it's quicker to generate multiple
-    // builds from a single configuration where possible, using
-    // an array for the `output` option, where we can specify
-    // `file` and `format` for each target)
-    {
-        input: 'src/index.js',
-        external: ['ms'],
-        output: [
-            { file: pkg.main, format: 'cjs' },
-            { file: pkg.module, format: 'es' }
-        ]
-    }
-];
+        { 
+            file: pkg.module,
+            format: 'es' 
+        }, 
+        {
+            file: pkg.main,
+            format: 'cjs'
+        }        
+    ],
+};
--- src/utils/collection_utils.js
+++ src/utils/collection_utils.js
@@ -1,5 +1,4 @@
 export function safeGet(obj, propertyPath){
-    console.log("in safeGet with ", obj, propertyPath);
     const keys = propertyPath.split(".");
     let currVal = obj;
     for(let i=0; i < keys.length; i++){
